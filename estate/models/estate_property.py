56bf80b4687551f291f840e74d5604328cbe6f17
Ezra Kurniadi <ezrakurniadi@gmail.com>
2024-09-17 15:12:50 +0700

    Update estate_property model

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -10,4 +10,21 @@ class EstateProperty(models.Model):
     _order = "sequence"
 
     name = fields.Char()
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float()
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.selection([
+        ('north', 'North'),
+        ('east', 'East'),
+        ('south', 'South'),
+        ('west', 'West'),
+    ])
     
