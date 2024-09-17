069dc7e8d2a3cc67859cbb55900f26d2a342a77a
Ezra Kurniadi <ezrakurniadi@gmail.com>
2024-09-17 14:51:49 +0700

    estate property models

---
+++ estate/models/estate_property.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Estate Property Tutorial"
+    _order = "sequence"
+
+    name = fields.Char()
+    
