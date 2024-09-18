b6c9353e2365286e03b907b9920f4750c0dffa29
Ezra Kurniadi <ezrakurniadi@gmail.com>
2024-09-18 16:18:59 +0700

    update manifest

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,6 +4,9 @@
     'depends': ['base'],
     'author': "Ezra",
     'category': 'Sales/CRM',
+    'data': [
+        'security/ir.model.access.csv',
+    ],
     'installable': True,
     'application': True,    
 }
