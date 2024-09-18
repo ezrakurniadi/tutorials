0ee4e956bfaf46adb9ca56dec6d5fbcade325fbf
Ezra Kurniadi <ezrakurniadi@gmail.com>
2024-09-18 17:01:34 +0700

    Chapter 5 - Menus Rev 3

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,9 +6,9 @@
     'category': 'Sales/CRM',
     'data': [
         'security/ir.model.access.csv',
-        
-        'views/estate_property_views.xml',        
         'views/estate_menus.xml',
+        'views/estate_property_views.xml',        
+        
     ],
     'installable': True,
     'application': True,    
